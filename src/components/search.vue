c18b421c0da3e16618aeeef0fa7859d29b1dffc6
guanhjukvbg <guanabc66-6@qq.com>
2024-09-24 07:54:37 +0800

    feat：工具+搜索

---
+++ src/components/search.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="search" ></div>
+</template>
+<script>
+
+</script>
+<style>
+.search{
+    height:90vh;
+    width:100%
+}
+</style>
---
+++ src/views/text-search.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="person"><person/></div>
+    <div class="editor"><tinymce/></div>
+</template>
+<script setup>
+import person from '@/components/person.vue'
+import tinymce from '@/components/tinymce.vue'
+import search from '@/components/search.vue'
+</script>
+<style>
+.editor{
+    width: 71%;
+    height: 90%;
+    margin-left: 2.5%;
+}
+</style>
